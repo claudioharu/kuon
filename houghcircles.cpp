6055affaa2ae1973815208d633a215634315049b
claudioharu <claudioharu@gmail.com>
2014-05-03 20:29:38 -0300

    rgb detection

---
+++ houghcircles.cpp
@@ -0,0 +1,56 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
+
+using namespace cv;
+
+template <typename T>  void printMatTemplate(const Mat& M, bool isInt = true);
+void printMat(const Mat& M);
+
+/** @function main */
+int main(int argc, char** argv)
+{
+  Mat src, src_gray;
+
+  /// Read the image
+  src = imread( argv[1], 1 );
+
+  if( !src.data )
+    { return -1; }
+
+  medianBlur(src, src, 5);
+
+  /// Convert it to gray
+  cvtColor( src, src_gray, CV_BGR2GRAY );
+
+  /// Reduce the noise so we avoid false circle detection
+  //GaussianBlur( src_gray, src_gray, Size(9, 9), 2, 2 );
+
+  vector<Vec3f> circles;
+
+  /// Apply the Hough Transform to find the circles
+  HoughCircles( src_gray, circles, CV_HOUGH_GRADIENT, 1, 220, 188, 101, 0, 500 );
+
+  /// Draw the circles detected
+  for( size_t i = 0; i < circles.size(); i++ )
+  {
+      Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+      int radius = cvRound(circles[i][2]);
+      
+      Vec3b bgrPixel = src.at<Vec3b>(center.y, center.x);
+      std::cout << bgrPixel << "," << center.x << "," << center.y << "\n";
+      
+      // circle center
+      circle( src, center, 3, Scalar(0,255,0), -1, 8, 0 );
+      // circle outline
+      circle( src, center, radius, Scalar(0,0,255), 3, 8, 0 );
+   }
+
+  /// Show your results
+  namedWindow( "Hough Circle Transform Demo", CV_WINDOW_NORMAL );
+  imshow( "Hough Circle Transform Demo", src );
+
+  waitKey(0);
+  return 0;
+}
