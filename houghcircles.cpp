130a6383e10d63d94cd7eecff5dc2366cdaa88d2
claudioharu <claudioharu@gmail.com>
2014-05-05 14:47:21 -0300

    50 cents

--- houghcircles.cpp
+++ houghcircles.cpp
@@ -11,17 +11,85 @@ unsigned long rgb2long(int r, int g, int b)
     return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
 }
 
+/*
+Calculates a table of 256 assignments with the given number of distinct values.
+
+Values are taken at equal intervals from the ranges [0, 128) and [128, 256),
+such that both 0 and 255 are always included in the range.
+*/
+cv::Mat lookupTable(int levels) {
+    int factor = 256 / levels;
+    cv::Mat table(1, 256, CV_8U);
+    uchar *p = table.data;
+
+    for(int i = 0; i < 128; ++i) {
+        p[i] = factor * (i / factor);
+    }
+
+    for(int i = 128; i < 256; ++i) {
+        p[i] = factor * (1 + (i / factor)) - 1;
+    }
+
+    return table;
+}
+
+/*
+Truncates channel levels in the given image to the given number of
+equally-spaced values.
+
+Arguments:
+
+image
+    Input multi-channel image. The specific color space is not
+    important, as long as all channels are encoded from 0 to 255.
+
+levels
+    The number of distinct values for the channels of the output
+    image. Output values are drawn from the range [0, 255] from
+    the extremes inwards, resulting in a nearly equally-spaced scale
+    where the smallest and largest values are always 0 and 255.
+
+Returns:
+
+Multi-channel images with values truncated to the specified number of
+distinct levels.
+*/
+cv::Mat colorReduce(const cv::Mat &image, int levels) {
+    cv::Mat table = lookupTable(levels);
+
+    std::vector<cv::Mat> c;
+    cv::split(image, c);
+    for (std::vector<cv::Mat>::iterator i = c.begin(), n = c.end(); i != n; ++i) {
+        cv::Mat &channel = *i;
+        cv::LUT(channel.clone(), table, channel);
+    }
+
+    cv::Mat reduced;
+    cv::merge(c, reduced);
+    return reduced;
+}
 
 /**Detection of 1R$**/
 bool oneReal(unsigned long centerColor, unsigned long radiusColor, int radius)
 {
 	if (radius >= 143 && radius <= 154)
 	{
-		return true;
-		//unsigned long sub = centerColor - radiusColor;
-		//if (sub < 0) sub = -sub;
-		//if (sub > 100000) return true;
-		//else return false;
+		if(centerColor == 0)
+			if (radiusColor > 0) return true;
+			else return false;
+	}
+	else return false;
+}
+
+/**Detection of 0,50R$**/
+bool halfReal(unsigned long centerColor, unsigned long radiusColor, int radius)
+{
+	if (radius >= 120 && radius <= 135)
+	{
+		if(centerColor == 0)
+			if (radiusColor == 0) return true;
+			else return false;
+		else return false;
 	}
 	else return false;
 }
@@ -47,8 +115,11 @@ int main(int argc, char** argv)
   cvtColor( src, src_hsv, CV_BGR2HSV );
 
   /// Reduce the noise so we avoid false circle detection
+  
+  medianBlur(src_hsv, src_hsv, 51);
   //GaussianBlur( src_hsv, src_hsv, Size(9, 9), 2, 2 );
-  medianBlur(src_hsv, src_hsv, 5);
+  src_hsv = colorReduce(src_hsv, 2);
+  
   
   vector<Vec3f> circles;
 
@@ -70,8 +141,8 @@ int main(int argc, char** argv)
       /// Store the rgb channels into a single number
       unsigned long bgrCenterHsv2long = rgb2long(bgrCenterHsv[0], bgrCenterHsv[1], bgrCenterHsv[2]);
       
-      std::cout << "rgb: "<< bgrCenterHsv2long << "	," << center.x << "," << center.y << "\n";
-      //std::cout << "hue: "<< hsvCenter[0] << "," << center.x << "," << center.y << "\n";
+	   std::cout << "rgb: "<< bgrCenterHsv2long << "	," << center.x << "," << center.y << "\n";
+      //std::cout << "rgb: "<< bgrCenterHsv << "," << center.x << "," << center.y << "\n";
       
       /// Radius color information
       //Vec3b bgrRadius = src.at<Vec3b>(center.y, center.x+radius);
@@ -84,6 +155,7 @@ int main(int argc, char** argv)
       
       std::cout << "rgb: "<< bgrRadiusHsv2long << "	," << center.x+radius << "," << center.y << " radius: "<< radius<<"\n\n";
       
+      //printf( "sub %ul" , bgrCenterHsv2long - bgrRadiusHsv2long); printf("\n\n");
       std::cout << "Eh Um real:" << oneReal(bgrCenterHsv2long, bgrRadiusHsv2long, radius) <<"\n\n";
       //std::cout << bgrRadius << "," << center.x+radius << "," << center.y << " radius: "<< radius<<"\n\n";
       
@@ -95,7 +167,7 @@ int main(int argc, char** argv)
 
   /// Show your results
   namedWindow( "Hough Circle Transform Demo", CV_WINDOW_NORMAL );
-  imshow( "Hough Circle Transform Demo", src );
+  imshow( "Hough Circle Transform Demo", src_hsv );
 
   waitKey(0);
   return 0;
